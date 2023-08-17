f700635eec78b06608c5e7964609f58de85a377b
Garrett Hanberg <115447682+garretthanberg@users.noreply.github.com>
2023-08-16 22:23:20 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvVhvLFEqhGSjGEiUozSFXupjId9zuKPE",
-  authDomain: "netflix-clone-gh.firebaseapp.com",
-  projectId: "netflix-clone-gh",
-  storageBucket: "netflix-clone-gh.appspot.com",
-  messagingSenderId: "709672707879",
-  appId: "1:709672707879:web:144bd8df808e105603eb02",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
